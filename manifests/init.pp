0f84bdb603a0cc0c9a7e292995af191db034a8d4
root <root@puppet.srce.hr>
2015-12-04 22:53:39 +0100

    config change

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,20 +11,7 @@
 # Sample Usage:
 #
 
-class simplesamlphp (
-  $authmemcookie         = $simplesamlphp::params::authmemcookie,
-  $package_ensure        = $simplesamlphp::params::package_ensure,
-  $alias                 = $simplesamlphp::params::alias,
-  $directory             = $simplesamlphp::params::directory,
-  $username              = $simplesamlphp::params::username,
-  $idp                   = $simplesamlphp::params::idp,
-  $proxyidp              = $simplesamlphp::params::proxyidp,
-  $fedlabidp             = $simplesamlphp::params::fedlabidp,
-  $authadminpassword     = $simplesamlphp::params::authadminpassword,
-  $secretsalt            = $simplesamlphp::params::secretsalt,
-  $technicalcontactname  = $simplesamlphp::params::technicalcontactname,
-  $technicalcontactemail = $simplesamlphp::params::technicalcontactemail,
-) inherits ::simplesamlphp::params {
+class simplesamlphp { 
 
   class{'simplesamlphp::package': } ->
   class{'simplesamlphp::config':  } ->
