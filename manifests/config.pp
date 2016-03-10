0d6ff5574d09216c043a6a4e6fe671649d07c558
Zavisa Nikac <znikac@srce.hr>
2016-03-10 23:08:04 +0100

    add ability to use sql datastore

--- manifests/config.pp
+++ manifests/config.pp
@@ -17,13 +17,13 @@ class simplesamlphp::config {
     mode    => $simplesamlphp::file_mode,
   }
 
-  file { $simplesamlphp::simplesamlphp_conf_file:
-    ensure  => file,
-    content => epp($simplesamlphp::simplesamlphp_conf_epp),
-    owner   => $simplesamlphp::file_owner,
-    group   => $simplesamlphp::file_group,
-    mode    => $simplesamlphp::file_mode,
-  }
+  #  file { $simplesamlphp::simplesamlphp_conf_file:
+  #  ensure  => file,
+  #  content => epp($simplesamlphp::simplesamlphp_conf_epp),
+  #  owner   => $simplesamlphp::file_owner,
+  #  group   => $simplesamlphp::file_group,
+  #  mode    => $simplesamlphp::file_mode,
+  #}
 
   file { $simplesamlphp::saml20_idp_remote_php_file:
     ensure  => file,
@@ -33,11 +33,11 @@ class simplesamlphp::config {
     mode    => $simplesamlphp::file_mode,
   }
 
-  exec { 'reload apache service':
-    subscribe   => File[$simplesamlphp::simplesamlphp_conf_file],
-    command     => $simplesamlphp::apache_reload,
-    refreshonly => true,
-  }
+  #  exec { 'reload apache service':
+  #   subscribe   => File[$simplesamlphp::simplesamlphp_conf_file],
+  # command     => $simplesamlphp::apache_reload,
+  # refreshonly => true,
+  #}
 
   if $simplesamlphp::authmemcookie == true {
 
@@ -65,11 +65,11 @@ class simplesamlphp::config {
           }
       }
       'Debian' : {
-        file { $simplesamlphp::authmemcookie_conf_file:
-          ensure => link,
-          target => $simplesamlphp::authmemcookie_conf_epp,
+                file { $simplesamlphp::authmemcookie_conf_file:
+         ensure => link,
+         target => $simplesamlphp::authmemcookie_conf_epp,
         }
-        exec { 'reload apache2 service for authmemcookie':
+                exec { 'reload apache2 service for authmemcookie':
           subscribe   => File[$simplesamlphp::authmemcookie_conf_file],
           command     => $simplesamlphp::apache_reload,
           refreshonly => true,
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,37 +1,42 @@
 # Class: simplesamlphp::params
 class simplesamlphp::params {
 
-  $authmemcookie         = false
-  $package               = 'simplesamlphp-aai'
-  $package_ensure        = 'present'
-  $pkgs_ensure           = 'present'
-  $simplesamlalias       = 'simplesaml'
-  $directory             = '/var/www/html/'
-  $username              = 'hrEduPersonUniqueID'
-  $idp                   = 'https://login.aaiedu.hr/sso/saml2/idp/metadata.php'
-  $proxyidp              = 'https://login.aaiedu.hr/proxy/saml2/idp/metadata.php'
-  $fedlabidp             = 'fed-lab.aaiedu.hr'
-  $authadminpassword     = 'PromijeniMe'
-  $secretsalt            = 'x1020wd03d24webk02pujzbenkbmeffg'
-  $technicalcontactname  = 'Administrator'
-  $technicalcontactemail = 'na@example.org'
-  $file_owner            = 'root'
-  $file_mode             = '0644'
+  $authmemcookie              = false
+  $package                    = 'simplesamlphp-aai'
+  $package_ensure             = 'present'
+  $pkgs_ensure                = 'present'
+  $simplesamlalias            = 'simplesaml'
+  $directory                  = '/var/www/html/'
+  $username                   = 'hrEduPersonUniqueID'
+  $idp                        = 'https://login.aaiedu.hr/sso/saml2/idp/metadata.php'
+  $proxyidp                   = 'https://login.aaiedu.hr/proxy/saml2/idp/metadata.php'
+  $fedlabidp                  = 'fed-lab.aaiedu.hr'
+  $authadminpassword          = 'PromijeniMe'
+  $secretsalt                 = 'x1020wd03d24webk02pujzbenkbmeffg'
+  $technicalcontactname       = 'Administrator'
+  $technicalcontactemail      = 'na@example.org'
+  $store_type                 = 'phpsession'
+  $store_sql_dsn              = 'sqlite:/path/to/sqlitedatabase.sq3'
+  $store_sql_username         = 'null'
+  $store_sql_password         = 'null'
+  $store_sql_prefix           = 'simpleSAMLphp'
+  $file_owner                 = 'root'
+  $file_mode                  = '0644'
+  $authsources_php_file       = '/usr/share/simplesamlphp-aai/config/authsources.php'
+  $authsources_php_epp        = 'simplesamlphp/authsources.php.epp'
+  $config_php_file            = '/usr/share/simplesamlphp-aai/config/config.php'
+  $config_php_epp             = 'simplesamlphp/config.php.epp'
+  $saml20_idp_remote_php_file = '/usr/share/simplesamlphp-aai/metadata/saml20-idp-remote.php'
+  $saml20_idp_remote_php_src  = 'puppet:///modules/simplesamlphp/saml20-idp-remote.php'
+  $authmemcookie_php_file     = '/usr/share/simplesamlphp-aai/config/authmemcookie.php'
+  $authmemcookie_php_epp      = 'simplesamlphp/authmemcookie.php.epp'
 
   case $facts['os']['family'] {
     'RedHat' : {
       $support_pkgs               = ['memcached', 'mod_auth_memcookie', 'php-pecl-memcache']
       $file_group                 = 'apache'
-      $authsources_php_file       = '/usr/share/simplesamlphp-aai/config/authsources.php'
-      $authsources_php_epp        = 'simplesamlphp/centos/authsources.php.epp'
-      $config_php_file            = '/usr/share/simplesamlphp-aai/config/config.php'
-      $config_php_epp             = 'simplesamlphp/centos/config.php.epp'
-      $saml20_idp_remote_php_file = '/usr/share/simplesamlphp-aai/metadata/saml20-idp-remote.php'
-      $saml20_idp_remote_php_src  = 'puppet:///modules/simplesamlphp/centos/saml20-idp-remote.php'
       $simplesamlphp_conf_file    = '/etc/httpd/conf.d/simplesamlphp.conf'
       $simplesamlphp_conf_epp     = 'simplesamlphp/centos/simplesamlphp.conf.epp'
-      $authmemcookie_php_file     = '/usr/share/simplesamlphp-aai/config/authmemcookie.php'
-      $authmemcookie_php_epp      = 'simplesamlphp/centos/authmemcookie.php.epp'
       $authmemcookie_conf_file    = '/etc/httpd/conf.d/authmemcookie.conf'
       $authmemcookie_conf_epp     = 'simplesamlphp/centos/authmemcookie.conf.epp'
       
@@ -41,23 +46,15 @@ class simplesamlphp::params {
       }
     }
     'Debian': {
-      $support_pkgs               = ['memcached', 'libapache2-mod-auth-memcookie']
+      $support_pkgs               = ['memcached', 'libmemcache0']
       $file_group                 = 'www-data'
-      $authsources_php_file       = '/etc/simplesamlphp/authsources.php'
-      $authsources_php_epp        = 'simplesamlphp/debian/authsources.php.epp'
-      $config_php_file            = '/etc/simplesamlphp/config.php'
-      $config_php_epp             = 'simplesamlphp/debian/config.php.epp'
-      $saml20_idp_remote_php_file = '/etc/simplesamlphp/metadata/saml20-idp-remote.php'
-      $saml20_idp_remote_php_src  = 'puppet:///modules/simplesamlphp/debian/saml20-idp-remote.php'
       $simplesamlphp_conf_file    = '/etc/apache2/conf.d/simplesamlphp-aai.conf'
       $simplesamlphp_conf_epp     = 'simplesamlphp/debian/simplesamlphp.conf.epp'
-      $authmemcookie_php_file     = '/etc/simplesamlphp/authmemcookie.php'
-      $authmemcookie_php_epp      = 'simplesamlphp/debian/authmemcookie.php.epp'
       $authmemcookie_conf_file    = '/etc/apache2/mods-enabled/auth_memcookie.load'
       $authmemcookie_conf_epp     = '/etc/apache2/mods-available/auth_memcookie.load'
 
       case $facts['os']['release']['major'] {
-        '8'     : { $apache_reload = '/bin/systemctl  reload apache2' }
+        '8'     : { $apache_reload = '/bin/systemctl reload apache2' }
         default : { $apache_reload = '/usr/sbin/service apache2 reload' }
       }
 
