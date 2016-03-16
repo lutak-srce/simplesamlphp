8d4c68f930e62d56309b5a5d5df8e69407c89bea
Zavisa Nikac <znikac@srce.hr>
2016-03-16 14:06:42 +0100

    use name instead of family for Debian

--- manifests/init.pp
+++ manifests/init.pp
@@ -50,7 +50,7 @@ class simplesamlphp (
   String         $apache_reload              = $simplesamlphp::params::apache_reload,
 ) inherits ::simplesamlphp::params {
 
-  if (($facts['os']['family'] == 'RedHat') and ($facts['os']['release']['major'] =~ /[5-7]/)) or (($facts['os']['family'] == 'Debian') and ($facts['os']['release']['major'] == '8')) {
+  if (($facts['os']['family'] == 'RedHat') and ($facts['os']['release']['major'] =~ /[5-7]/)) or (($facts['os']['name'] == 'Debian') and ($facts['os']['distro']['codename'] == 'jessie')) {
 
     class{'simplesamlphp::package': } ->
     class{'simplesamlphp::config' : } ->
