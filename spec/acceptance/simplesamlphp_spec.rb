0f8d1e2b63303ad5953bed2fe211bb534617d2fe
Zavisa Nikac <znikac@srce.hr>
2016-10-17 17:44:51 +0200

    initial acceptance tests

--- spec/acceptance/simplesamlphp_spec.rb
+++ spec/acceptance/simplesamlphp_spec.rb
@@ -1,15 +1,21 @@
 require 'spec_helper_acceptance'
 
-describe 'simplesamlphp' do
-  describe 'running puppet code' do
-    it 'should work with no errors' do
+describe 'simplesamlphp class' do
+
+  context 'default parameters' do
+    it 'should work idempotently with no errors' do
       pp = <<-EOS
-        class { 'simplesamlphp': }
+      class { 'simplesamlphp': }
       EOS
 
       # Run it twice and test for idempotency
       apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes => true)
+      apply_manifest(pp, :catch_changes  => true)
     end
+
+    describe package('simplesamlphp-aai') do
+      it { is_expected.to be_installed }
+    end
+
   end
 end
