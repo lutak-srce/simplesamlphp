4b48fe298000e5cec68f9b529882d7d0b7419384
Zavisa Nikac <znikac@srce.hr>
2016-10-18 13:31:18 +0200

    initial acceptance tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,14 +1,13 @@
-require 'beaker-rspec'
+require 'beaker-rspec/spec_helper'
 require 'beaker/puppet_install_helper'
 
 hosts.each do |host|
   # Install Puppet
-  on host, install_puppet
+  install_puppet
 end
 
 RSpec.configure do |c|
   module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-  module_name = 'simplesamlphp'
 
   # Readable test descriptions
   c.formatter = :documentation
@@ -16,6 +15,6 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module
-    puppet_module_install(:source => module_root, :module_name => module_name)
+    puppet_module_install(:source => module_root, :module_name => 'simplesamlphp')
   end
 end
