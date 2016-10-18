1372f3b85df51f8290570fd8f10142c454657802
Zavisa Nikac <znikac@srce.hr>
2016-10-18 18:04:56 +0200

    initial acceptance tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -22,6 +22,7 @@ RSpec.configure do |c|
 
       destdir = "#{environmentpath}/production/modules"
       on host, "git clone -b initial_spec_tests https://github.com/lutak-srce/simplesamlphp #{destdir}/simplesamlphp"
+      on host, "git clone -b master https://github.com/lutak-srce/aptrepo #{destdir}/aptrepo"
       on host, puppet('module', 'install', 'puppetlabs-apt'), { :acceptable_exit_codes => [0,1] }
     end
 
