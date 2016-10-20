02d057173e951c50940add49edaa357453cff3a0
Zavisa Nikac <znikac@srce.hr>
2016-10-20 20:23:41 +0200

    add srce repos

--- spec/acceptance/simplesamlphp_spec.rb
+++ spec/acceptance/simplesamlphp_spec.rb
@@ -5,6 +5,25 @@ describe 'simplesamlphp class' do
   context 'default parameters' do
     it 'should work idempotently with no errors' do
       pp = <<-EOS
+        if $::osfamily == 'Debian' {
+          class { 'apt': }
+          apt::source { 'srce-repo':
+            key         => 'E2FFF7957AEC9D5118B95BE2FECB42104089CBA3',
+            key_server  => 'pks.aaiedu.hr',
+            location    => 'http://ftp.srce.hr/srce-debian/',
+            release     => "srce-${::lsbdistcodename}",
+            repos       => 'main',
+            include_src => false,
+            before      => Class['simplesamlphp'],
+          }
+        } elsif $::osfamily == 'RedHat' {
+         yumrepo { 'srce-repo':
+          baseurl  => "http://ftp.srce.hr/redhat/base/$operatingsystemrelease/$architecture/",
+            enabled  => 1,
+            gpgcheck => 0,
+            before   => Class['simplesamlphp'],
+          }
+        }
         class { 'simplesamlphp': 
           package=> 'simplesamlphp-aai'
         }
