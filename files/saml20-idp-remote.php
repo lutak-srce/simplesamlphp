44a46e7882832884db18c10e4ea9266a9abb08b6
Zaviša Nikač <zavisa.nikac@srce.hr>
2017-06-12 12:24:48 +0200

    add support for authentication through social networks

--- files/saml20-idp-remote.php
+++ files/saml20-idp-remote.php
@@ -76,3 +76,27 @@ $metadata['fed-lab.aaiedu.hr'] = array (
 );
 
 
+$metadata['https://login.aaiedu.hr/proxy/saml2/idp/metadata.php'] = array (
+  'metadata-set' => 'saml20-idp-remote',
+  'entityid' => 'https://login.aaiedu.hr/proxy/saml2/idp/metadata.php',
+  'SingleSignOnService' => 
+  array (
+    0 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+      'Location' => 'https://login.aaiedu.hr/proxy/saml2/idp/SSOService.php',
+    ),
+  ),
+  'SingleLogoutService' => 
+  array (
+    0 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+      'Location' => 'https://login.aaiedu.hr/proxy/saml2/idp/SingleLogoutService.php',
+    ),
+  ),
+  'certData' => 'MIIHfjCCBmagAwIBAgIQCWGsVxLC3qXbHbpSfkUV4DANBgkqhkiG9w0BAQsFADBzMQswCQYDVQQGEwJOTDEWMBQGA1UECBMNTm9vcmQtSG9sbGFuZDESMBAGA1UEBxMJQW1zdGVyZGFtMQ8wDQYDVQQKEwZURVJFTkExJzAlBgNVBAMTHlRFUkVOQSBTU0wgSGlnaCBBc3N1cmFuY2UgQ0EgMzAeFw0xNTA3MjMwMDAwMDBaFw0xNzA3MjcxMjAwMDBaMIIBFDEaMBgGA1UEDwwRR292ZXJubWVudCBFbnRpdHkxEzARBgsrBgEEAYI3PAIBAxMCSFIxGjAYBgNVBAUTEUdvdmVybm1lbnQgRW50aXR5MRwwGgYDVQQJDBNKb3NpcGEgTWFyb2huacSNYSA1MREwDwYDVQQREwhIUi0xMDAwMDELMAkGA1UEBhMCSFIxDzANBgNVBAgTBlphZ3JlYjEPMA0GA1UEBxMGWmFncmViMUswSQYDVQQKDEJTUkNFIChTdmV1xI1pbGnFoXRlIHUgWmFncmVidSBTdmV1xI1pbGnFoW5pIHJhxI11bnNraSBjZW50YXIgU3JjZSkxGDAWBgNVBAMTD2xvZ2luLmFhaWVkdS5ocjCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKcu6B2cF+9YN2s1ui2vnC1IviEN9EoXgOgUMtkA6XmHPAnw/PJnTaqu5vWTIPNWyktGB/nbMeP0kX7rNV3sryaRZ2HhG345OBzG6MJIJ/C+woWnqW6VbxIxjhPedVrQ5kYYZqyO2JsTmSx+G/oCNHFZDOlIvx+Fp4Wx+PPyOWsxLTpcRYTLJJw5JCorJWuLHJ8wEIeugp9/gUzzA9txq6x/UcOX6keBYhOircofMfuZulHUXqs+vZVeEecLGjuNi1g4oOv8QNtEAKjOURrywhfmiSYq9rV/y7d2arhMOxhJun5prxZLrIhaOZbY9AwFk633n8g0txcjiZ8mPG2Ut0kCAwEAAaOCA2kwggNlMB8GA1UdIwQYMBaAFMK4hdfhuRO90Ui8/V7cfZBCeoqpMB0GA1UdDgQWBBQ+WPibu9hc7mgiZb8D2RGKVP0/UjAaBgNVHREEEzARgg9sb2dpbi5hYWllZHUuaHIwDgYDVR0PAQH/BAQDAgWgMB0GA1UdJQQWMBQGCCsGAQUFBwMBBggrBgEFBQcDAjCBhQYDVR0fBH4wfDA8oDqgOIY2aHR0cDovL2NybDMuZGlnaWNlcnQuY29tL1RFUkVOQVNTTEhpZ2hBc3N1cmFuY2VDQTMuY3JsMDygOqA4hjZodHRwOi8vY3JsNC5kaWdpY2VydC5jb20vVEVSRU5BU1NMSGlnaEFzc3VyYW5jZUNBMy5jcmwwQgYDVR0gBDswOTA3BglghkgBhv1sAgEwKjAoBggrBgEFBQcCARYcaHR0cHM6Ly93d3cuZGlnaWNlcnQuY29tL0NQUzB7BggrBgEFBQcBAQRvMG0wJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmRpZ2ljZXJ0LmNvbTBFBggrBgEFBQcwAoY5aHR0cDovL2NhY2VydHMuZGlnaWNlcnQuY29tL1RFUkVOQVNTTEhpZ2hBc3N1cmFuY2VDQTMuY3J0MAwGA1UdEwEB/wQCMAAwggF/BgorBgEEAdZ5AgQCBIIBbwSCAWsBaQB3AKS5CZC0GFgUh7sTosxncAo8NZgE+RvfuON3zQ7IDdwQAAABTrz6Iw4AAAQDAEgwRgIhAO57T4MBDf3n4/vuT4JLvxk7D380fvCNGWQZi86jkmugAiEAo/k/DqV6bd94tUV0r7dMg3lznfy34cNf3J06XuTZgGkAdQBo9pj4H2SCvjqM7rkoHUz8cVFdZ5PURNEKZ6y7T0/7xAAAAU68+iMQAAAEAwBGMEQCICONwqhLeEzqCn/rsay9BRcftidBFORjDC1rlAJmL9pNAiBaNsqGb9leF0Lq5H5MytKXK4+5BXVPnge8BU3cJzUIRgB3AFYUBpov18Ls0/XhvUSyPsdGdrm8mRFcwO+UmFXWidDdAAABTrz6JG0AAAQDAEgwRgIhANpcqj6MYPfs6UUPvaqgZKUdayRJEcvZN6bK/vaAa1nIAiEA3oUjRCEc+MByNewW8MW/7J0hOWfVbiEYyXl8v7ytP+0wDQYJKoZIhvcNAQELBQADggEBABMB006qcQzAHCAKdWnqt8ePx7A+jTGc6iCOP8XHz7UA4aIQL6jeurfQUUnFM3nh+Bfiue1wVsbPu1GKxvwVTrrtO8jXuajzhG9M+z9m6dTtpqCGfMqkpVzMQNcpCFE+giICytsxPFoalkiWBmrygCFY4Nd5dVg70ZIgefhqROuJI5jvWORWrl50nt33A9BgFVv4gdswYcf5VHuPCREHXG/mNe5BgDdgEOUVbhIlcsd7HnHK55C3lQ6//VDdFrrB69+5oJrZo5Od8kbX2ptmdkT2bT2qiKvVdb8WpL9PiHWlVcmAfT7QJOBoFWoN92sxxExoDjRws+/IoNlOMiIPnGQ=',
+  'NameIDFormat' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
+);
+
+
